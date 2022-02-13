3090cd98f2b53bca3b46e7d03bfb57d4f5e23c30
ref1821 <ref1821@gmail.com>
2022-02-13 16:08:21 -0600

    c fuera del origen 1

--- src/circulo/Circulo.jsx
+++ src/circulo/Circulo.jsx
@@ -17,13 +17,16 @@ const Circulo = () => {
     }
   return (
   <div>
-      <h2>ecuación del circulo con el centro desde el origen</h2>
-      <h3>radio</h3>
-      <input type="number" placeholder="r" name="r" onChange={handleInputChange} className='inputc'/>
-      <h4>x^2+y^2={datos.r*datos.r}</h4>
-      <h3>radio incognita</h3>
-      x+y+<input type="number" placeholder="#" name="ri" onChange={handleInputChange} className='inputc' id='inputc'/>
-      <h2>r={Math.sqrt(datos.ri)}</h2>
+    <h2>ecuación del circulo con el centro desde el origen</h2>
+    <h3>radio</h3>
+    <input type="number" placeholder="r" name="r" onChange={handleInputChange} className='inputc'/>
+    <h4>x^2+y^2={datos.r*datos.r}</h4>
+    <h3>radio incognita</h3>
+    x+y+<input type="number" placeholder="#" name="ri" onChange={handleInputChange} className='inputc' id='inputc'/>
+    <h2>r={Math.sqrt(datos.ri)}</h2>
+    <h2>ecuación del circulo con el centro fuera del origen</h2>
+    C(<input type="number" placeholder="h" name="h" onChange={handleInputChange} className='inputc'/>, <input type="number" placeholder="k" name="k" onChange={handleInputChange} className='inputc'/>)
+    <p>r<input type="number" placeholder="r" name="r" onChange={handleInputChange} className='inputc'/></p>
 
   </div>
   );
